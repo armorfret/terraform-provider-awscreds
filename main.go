f0d2bf6201e287b6a3f7300b0febbbeb48f9b9df
Les Aker <me@lesaker.org>
2023-05-30 22:46:42 -0400

    fix old URL

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	opts := &plugin.ServeOpts{ProviderFunc: provider.New()}
 
 	if debugMode {
-		err := plugin.Debug(context.Background(), "terraform.scrtybybscrty.org/armorfret/awscreds", opts)
+		err := plugin.Debug(context.Background(), "https://registry.terraform.io/providers/armorfret/awscreds", opts)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
